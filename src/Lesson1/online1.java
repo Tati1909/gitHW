80b2cd445667cc72f00d41ad93c5c01af5251ffd
DESKTOP-9NLO4QL\emuar <emuart155@gmail.com>
2021-01-29 19:41:01 +0300

    прошла лекция 1

---
+++ src/Lesson1/online1.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+public class online1 {
+    public static void main(String[] args) {
+       /* Cat c =  новый  Cat ();
+        Bird b =  новая  Bird ();
+        Змея s   =  новая  Змея ();
+        List < Moving > movings =  new  ArrayList <> ();
+        переезды . добавить (с);
+        переезды . добавить (б);
+        переезды . добавить (и);
+
+        for ( Movings m : movings) {
+            м . переехать();
+        } */
+    }
+}
